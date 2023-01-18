866156b686f0383c82d7c2e0570dfd2a2e3b9fbc
Anton <antonkhazin2006@gmail.com>
2023-01-19 00:02:24 +0300

    sides changed

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,4 @@
 export let poleSquare = [
-  { type: "castle", side: "black", active: false },
-  { type: "horse", side: "black", active: false },
-  { type: "bishop", side: "black", active: false },
-  { type: "queen", side: "black", active: false },
-  { type: "king", side: "black", active: false },
-  { type: "bishop", side: "black", active: false },
-  { type: "horse", side: "black", active: false },
-  { type: "castle", side: "black", active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
-  { type: "pawn", side: "black", firstMove: true, active: false },
   { type: "", side: "", active: false },
   { type: "", side: "", active: false },
   { type: "", side: "", active: false },
@@ -63,8 +47,22 @@ export let poleSquare = [
   { type: "", side: "", active: false },
   { type: "", side: "", active: false },
   { type: "", side: "", active: false },
-  { type: "", side: "", active: false },
-  { type: "", side: "", active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "pawn", side: "white", firstMove: true, active: false },
+  { type: "castle", side: "white", active: false },
+  { type: "horse", side: "white", active: false },
+  { type: "bishop", side: "white", active: false },
+  { type: "queen", side: "white", active: false },
+  { type: "king", side: "white", active: false },
+  { type: "bishop", side: "white", active: false },
+  { type: "horse", side: "white", active: false },
+  { type: "castle", side: "white", active: false },
 ];
 export function switchType(type) {
   switch (type) {
@@ -92,13 +90,17 @@ export function whereCanGo(mass, index) {
   let thisFigure = mass[index];
   switch (thisFigure.type) {
     case "pawn":
-      if (thisFigure.side == "black") {
-        if (mass[index + 8].type == "") {
-          mass[index + 8].active = true;
-          if (mass[index + 16].type == "" && thisFigure.firstMove) {
-            thisFigure.firstMove = false;
-            mass[index + 16].active = true;
-            console.log(mass);
+      console.log(mass.length);
+      console.log(index - 8);
+      if (index - 8 > 0) {
+        if (mass[index - 8].type == "") {
+          mass[index - 8].active = true;
+          if (index - 16 > 0) {
+            if (mass[index - 16].type == "" && thisFigure.firstMove) {
+              thisFigure.firstMove = false;
+              mass[index - 16].active = true;
+              console.log(mass);
+            }
           }
         }
       }
